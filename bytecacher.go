b5799070552427cd57fd55fbf62557335b672833
Frederick Robinson <frederick.robinson@gmail.com>
2019-04-27 18:22:29 +0200

    first commit

---
+++ bytecacher.go
@@ -0,0 +1,120 @@
+package bytecacher
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"sync"
+)
+
+type Cacher struct {
+	lookup func(string) ([]byte, error)
+	path   string
+
+	locksLock sync.RWMutex
+	locks     map[string]*sync.RWMutex
+}
+
+func NewCacher(
+	lookupFn func(string) ([]byte, error),
+	path string,
+) *Cacher {
+	cacher := &Cacher{
+		lookup: lookupFn,
+		path:   path,
+
+		locksLock: sync.RWMutex{},
+		locks:     map[string]*sync.RWMutex{},
+	}
+
+	return cacher
+}
+
+// getMx gets the mutex for a key, creating it if necessary
+func (c *Cacher) getMx(key string) *sync.RWMutex {
+	c.locksLock.RLock()
+	ans, ok := c.locks[key]
+	c.locksLock.RUnlock()
+
+	if ok {
+		return ans
+	}
+
+	c.locksLock.Lock()
+	defer c.locksLock.Unlock()
+
+	ans, ok = c.locks[key]
+	if ok {
+		return ans
+	}
+
+	ans = &sync.RWMutex{}
+	c.locks[key] = ans
+	return ans
+}
+
+func (c *Cacher) Get(key string) ([]byte, error) {
+	if ans, err := c.retrieve(key); err == nil {
+		return ans, nil
+	}
+
+	return c.store(key)
+}
+
+func (c *Cacher) retrieve(key string) ([]byte, error) {
+	mx := c.getMx(key)
+	mx.RLock()
+	defer mx.RUnlock()
+
+	path := c.filePath(key)
+	if !fileExists(path) {
+		return []byte{}, fmt.Errorf("not stored")
+	}
+
+	dat, err := ioutil.ReadFile(path)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return dat, nil
+}
+
+func (c *Cacher) store(key string) ([]byte, error) {
+	mx := c.getMx(key)
+	mx.Lock()
+	defer mx.Unlock()
+
+	ans, err := c.lookup(key)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	path := c.filePath(key)
+	err = ioutil.WriteFile(path, ans, 0644)
+
+	return ans, err
+}
+
+func (c *Cacher) filePath(key string) string {
+	return c.path + "/" + key
+}
+
+// fileExists reports if there is a file at the given path or not.
+func fileExists(filePath string) bool {
+
+	info, err := os.Stat(filePath)
+
+	if os.IsNotExist(err) {
+		return false
+	}
+
+	if err == nil {
+		return !info.IsDir()
+	}
+
+	log.Fatalf("encountered unhandled error %+v while statting file %s", err, filePath)
+
+	// never happens
+	return false
+}
